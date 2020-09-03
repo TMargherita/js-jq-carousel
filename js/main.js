bab2192a3da2ce49a0627fb37e194efbef98637e
Margherita <margherita.testi@gmail.com>
2020-09-03 10:31:25 +0200

    risolto problema visualizzazione

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-$(document).ready(function(
-  $(".images").click(
-    function () {
-        var imgActive = $("img.active");
-        imgActive.removeClass("active");
+$(document).ready(function() {
 
-        if (imgActive.hasClass("last") == true) {
-          var nextImg = $("img.first");
-        } else {
-          var nextImg = imgActive.next();
-        }
-        nextImg.addClass("active");
+  $(".next").click(
+    function() {
+      var imgActive = $("img.active");
+      imgActive.removeClass("active");
+
+      var circleActive = $("i.active");
+      circleActive.removeClass("active");
+
+      if(imgActive.hasClass("last")) {
+        var nextImg = $("img.first");
+        var nextCircle = $("i.first");
+      } else {
+        var nextImg = imgActive.next();
+        var nextCircle = circleActive.next();
       }
+
+      nextImg.addClass("active");
+      nextCircle.addClass("active");
+    }
   );
-)
+
+
+  $(".prev").click(
+    function() {
+      var imgActive = $("img.active");
+      imgActive.removeClass("active");
+
+      if(imgActive.hasClass("first")) {
+        var nextImg = $ ("img.first");
+      } else {
+        var nextImg = imgActive.prev();
+      }
+      nextImg.addClass("active");
+    }
+  );
+});
